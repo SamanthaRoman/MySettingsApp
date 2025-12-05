629f9d907c3b46bf68e1a0f42a41ff1ad10571fa
Samantha Roman <samantharaeroman@gmail.com>
2025-12-05 12:19:16 -0500

    Initial Commit

---
+++ MySettingsApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MySettingsApp
+//
+//  Created by Samantha Roman on 12/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MySettingsApp/MySettingsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySettingsAppApp.swift
+//  MySettingsApp
+//
+//  Created by Samantha Roman on 12/5/25.
+//
+
+import SwiftUI
+
+@main
+struct MySettingsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
